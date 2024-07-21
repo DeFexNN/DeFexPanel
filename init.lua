e6230fdd355aecce496da19e275fbdcb6fa9e93c
unknown <th.9.coc.den@gmail.com>
2024-07-21 06:53:07 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+--名称
+appname="ADMIN PANEL1"
+--版本号
+appver="1.0"
+--版本
+appcode="1"
+--SDK
+appsdk="15"
+--包名
+packagename="com.upluk.admi"
+--调试模式
+debugmode=nil
+--应用权限
+user_permission={
+  "DOWNLOAD_WITHOUT_NOTIFICATION",
+  "INTERNET",
+  "READ_EXTERNAL_STORAGE",
+  "WRITE_EXTERNAL_STORAGE",
+}
+--跳过编译
+skip_compilation={
+
+}
